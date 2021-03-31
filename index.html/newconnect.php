8b8afb91c68335b9e19bb4472bf2230067d73a5c
ktarjuna <81671960+ktarjuna@users.noreply.github.com>
2021-03-31 12:27:59 +0530

    Add files via upload

---
+++ index.html/newconnect.php
@@ -0,0 +1,26 @@
+<?php
+
+$user = 'marques';
+$pass = 'arjun123';
+$db_name = 'IBM AC922';
+$host = 'localhost';
+$name = $_POST['name'];
+$Contact = $_POST['tel'];
+$Email = $_POST['email'];
+$OrganizationName = $_POST['Organizaton'];
+$Website = $_POST['Enquiry'];
+
+$conn = mysqli_connect($host, $user, $pass,$db_name);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "INSERT INTO AC922 (Name,Contact,Email,Organization,Enquiry) VALUES ('$name','$Contact','$Email','$Organization','$Enquiry')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Thank You!! We'll reach you Shortly !";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
